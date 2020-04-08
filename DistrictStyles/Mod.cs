11eb7e03521e887a251b8236834328480c297ed6
boformer <felix.free@gmx.de>
2020-04-09 00:35:02 +0300

    project setup, add Harmony dependency

---
+++ DistrictStyles/Mod.cs
@@ -0,0 +1,16 @@
+﻿using ICities;
+
+namespace DistrictStyles {
+    public class Mod : IUserMod {
+        public string Name => "DistrictStyles+";
+        public string Description => "Advanced Zoning Policies for Mayors";
+
+        public void OnEnabled() {
+
+        }
+
+        public void OnDisabled() {
+
+        }
+    }
+}
