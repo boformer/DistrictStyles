e40e96150b9b55524d436092df7a1b092b243d1d
boformer <felix.free@gmx.de>
2020-04-09 00:37:23 +0300

    Adjust mod name to look better in content manager

--- DistrictStyles/Mod.cs
+++ DistrictStyles/Mod.cs
@@ -2,7 +2,7 @@
 
 namespace DistrictStyles {
     public class Mod : IUserMod {
-        public string Name => "DistrictStyles+";
+        public string Name => "District Styles Plus";
         public string Description => "Advanced Zoning Policies for Mayors";
 
         public void OnEnabled() {
